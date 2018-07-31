095fa0f1c80a5c45f69940d8c8cbc23fa777e895
Jemadean Dobrosielski <jemadean@gmail.com>
2018-07-31 14:50:54 -0700

    added error handling

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const express = require('express');
 
 const data = require('./db/notes');
 
+const { PORT } = require('./config');
+
+const {logIncomingRequests} = require('./middleware/logger');
+
 const app = express();
 
 console.log('Hello Noteful!');
@@ -12,6 +16,8 @@ console.log('Hello Noteful!');
 // INSERT EXPRESS APP CODE HERE...
 
 app.use(express.static('public'));
+
+app.use(logIncomingRequests);
 
 app.get('/api/notes', (req, res) => {
   const searchTerm = req.query.searchTerm;
@@ -28,7 +34,21 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(filteredData);
 });
 
-app.listen(8080, function () {
+app.use(function (req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
