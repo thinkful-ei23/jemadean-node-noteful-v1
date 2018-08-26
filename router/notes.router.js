862491c69063e474a252d7a09e93c8a91c7e8464
Jemadean Dobrosielski <jemadean@gmail.com>
2018-08-25 21:12:06 -0700

    added router

--- router/notes.router.js
+++ router/notes.router.js
@@ -4,9 +4,11 @@ const express = require('express');
 const router = express.Router();
 
 const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
 
 router.get('/notes', (req, res) => {
-  const searchTerm = req.query.searchTerm;
+  const searchTerm = req.query;
   let filteredData = data;
   if (searchTerm) {
     filteredData = filteredData.filter(item => JSON.stringify(item).includes(searchTerm));
@@ -18,6 +20,29 @@ router.get('/notes/:id', (req, res) => {
   const id = req.params.id;
   const filteredData = data.find(item => item.id === Number(id));
   res.json(filteredData);
+});
+
+router.post('/notes', (req, res, next) => {
+  const { title, content } =  req.body;
+
+  const newItem = { title, content };
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
 });
 
 module.exports = router;
